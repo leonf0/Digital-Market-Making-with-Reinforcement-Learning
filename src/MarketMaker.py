38c8b5e2e57021b6c6d3ac385ef5236fd1d900d1
Leon Forde <leonforde1201@gmail.com>
2025-12-31 17:31:58 +0100

    Update MarketMaker.py

--- src/MarketMaker.py
+++ src/MarketMaker.py
@@ -31,3 +31,35 @@ class MarketMaker:
         self.wealth_history.append(curr_wealth)
         self.inventory_history.append(self.inventory)
         return curr_wealth
+
+class NaiveMarketMaker(MarketMaker):
+    def __init__(self, spread=0.5):
+        super().__init__()
+        self.spread = spread
+
+    def get_quotes(self, curr_time, fair_price):
+        half = self.spread / 2
+        return fair_price - half, fair_price + half
+
+class AvellanedaStoikov(MarketMaker):
+    def __init__(self, T, sigma, gamma, k):
+        super().__init__()
+        self.T = T
+        self.sigma = sigma
+        self.gamma = gamma
+        self.kappa = k
+
+    def reservation_price(self, fair_price, t):
+        time_remaining = max(0, self.T - t)
+        return fair_price - self.inventory * self.gamma * self.sigma**2 * time_remaining
+
+    def optimal_spread(self, t):
+        time_remaining = max(0, self.T - t)
+        term1 = self.gamma * self.sigma**2 * time_remaining / 2
+        term2 = (2 / self.gamma) * np.log(1 + self.gamma / self.kappa)
+        return term1 + term2
+
+    def get_quotes(self, curr_time, fair_price):
+        mid = self.reservation_price(fair_price, curr_time)
+        half = self.optimal_spread(curr_time) / 2
+        return mid - half, mid + half
